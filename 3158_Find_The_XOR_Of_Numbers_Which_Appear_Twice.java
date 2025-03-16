47f24e69d15d5c1ff020b4542bbaf0784c2db6bd
Ananya Gupta <116813705+AnanyaGupta18@users.noreply.github.com>
2025-03-16 21:59:14 +0530

    Create 3158_Find_The_XOR_Of_Numbers_Which_Appear_Twice.java

---
+++ 3158_Find_The_XOR_Of_Numbers_Which_Appear_Twice.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int duplicateNumbersXOR(int[] nums) {
+        int n = nums.length;
+        int res = 0;
+        Arrays.sort(nums);
+        for(int i = 0; i < n - 1 ; i++){
+            if(nums[i] == nums[i+1]){
+                res ^= nums[i];
+            }
+        }
+        return res;
+    }
+}
